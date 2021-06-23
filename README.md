8e00f1095553c86ac3357247ab41685c5032ab6e
kokoichi <tt.300607@gmail.com>
2021-06-23 13:46:27 +0900

    refine: images scraping

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
-# pythonの練習かつ備忘録も兼ねて
+# python関連の細々したもの
 
-## 良いコードを書く方法
+## フォルダ説明
+
+
+### books
+
+## その他
+### 良いコードを書く方法
 - [PEP8](https://pep8-ja.readthedocs.io/ja/latest)
 - [リーダブルコード](http://libgen.gs/ads.php?md5=149128e47c5cea6be986bebf001ecc5a)
 
 
-## 疑問
-- 洋書ってなんでこんなに転がってるん？？
+### 疑問
 
--- scraping/images.py
+++ scraping/images.py
@@ -6,48 +6,29 @@ import re
 
 
 HEADERS = {"User-Agent": "Mozilla/5.0"}
-BASEURL = "http://blog.nogizaka46.com/"
+BASE_URL = "http://blog.nogizaka46.com/"
+IMG_FOLDER = "./imgs/"
 
 # あるメンバーのブログ中の写真すべてを保存する、さかのぼれるだけ戻る
-def scraping_allpic():
+def scraping_allpic(member_name):
     # メンバーURL
-    ## 名前の指定、'_'で区切る
-    # member_name = "ayame_tsutsui"
-    # member_name = "minami_hoshino"
-    member_name = "renka_iwamoto"
     ## urlでのアクセスは.で名前を区切るので変換している
     name_for_url = member_name.replace('_','.')
-    url = "http://blog.nogizaka46.com/" + name_for_url + "/"
+    url = BASE_URL + name_for_url + "/"
 
     # フォルダ作成
-    if not os.path.isdir(member_name):  # ”member_name”のフォルダがない場合
+    member_path = IMG_FOLDER + member_name
+    if not os.path.isdir(member_path):  # ”member_name”のフォルダがない場合
         print("creating folder")
-        os.mkdir(member_name)
+        os.makedirs(member_path)
 
     # 保存枚数カウント用
     cnt = 0
 
-    # BeautifulSoupオブジェクト生成
-    ## これでurllib.error.HTTPErrorを解決できるかと思ったけど、無理でした。
-    # headers = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0"}
-    # soup = BeautifulSoup(requests.get(
-    #     url, headers=HEADERS).content, 'html.parser')
-
-    # print(type(soup))
-    # print(soup)
-
-    # page1の処理
-    # 画像が置かれているhtmlを見つける
-    # for entry in soup.find_all("div", class_="entrybody"):  # 全てのentrybodyを取得
-    #     for img in entry.find_all("img"):  # 全てのimgを取得
-    #         cnt += 1
-    #         urllib.request.urlretrieve(
-    #             img.attrs["src"], "./" + member_name + "/" + member_name + "-" + str(cnt) + ".jpeg")
-
     page_list = check_page_list(name_for_url)
 
+    # page1のみ例外で扱わなくてもいいようにする
     page_list.insert(0, '1')
-    # page2以降の処理
     for i in page_list:
         ## このpのマックスが12っぽい、それ以上はみれない
         url_tmp = url + f'?p={i}'
@@ -61,26 +42,27 @@ def scraping_allpic():
                 cnt += 1
                 try:
                     urllib.request.urlretrieve(
-                        img.attrs["src"], "./" + member_name + "/" + member_name + "-" + str(cnt).zfill(3) + ".jpeg")
+                        img.attrs["src"], member_path + "/" + member_name + "-" + str(cnt).zfill(3) + ".jpeg")
                 except urllib.error.HTTPError as err:
                     print(err)
                 except KeyError as err:
                     print(err)
-    
+
     print(str(cnt) + " pictures was saved")
 
 # 人によってさかのぼれるページが違うので、それをチェックする
 def check_page_list(member_name):
-    url = BASEURL + member_name + "/"
+    url = BASE_URL + member_name + "/"
     soup = BeautifulSoup(requests.get(
         url, headers=HEADERS).content, 'html.parser')
     a = soup.find_all("div", class_="paginate")
-    
+
     str_html = str(a[0])
     result = re.findall(r'p=([\d]+)', str_html)
     return result[:-1]
 
 
 if __name__ == '__main__':
-    scraping_allpic()
+	## 名前の指定、'_'で区切る
+    scraping_allpic("renka_iwamoto")
 
